99a4dc0dced019d41dd93995488a3aa30ff87699
Clay Johnson <cjohnson@gradle.com>
2025-06-03 07:40:18 -0500

    We should not need these tool specific configs

--- .develocity.py
+++
@@ -1,6 +0,0 @@
-from develocity.config import from_properties_file, in_gradle_user_home
-
-def develocity_configuration(configuration):
-    configuration.develocity_url = 'https://ge.solutions-team.gradle.com'
-    configuration.access_key = from_properties_file(in_gradle_user_home())
-    return configuration
