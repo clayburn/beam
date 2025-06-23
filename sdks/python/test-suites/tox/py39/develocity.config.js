358118d61d64f57571b476ca6736641292e5b8a8
Clay Johnson <cjohnson@gradle.com>
2025-06-23 16:32:18 -0500

    Instrument typescript tests

--- sdks/python/test-suites/tox/py39/develocity.config.js
+++ sdks/python/test-suites/tox/py39/develocity.config.js
@@ -1,9 +1,6 @@
-const { fromPropertiesFile, inGradleUserHome } = require('@gradle/develocity-agent/api/config');
-
 module.exports = {
     projectId: 'beam',
     server: {
         url: 'https://ge.solutions-team.gradle.com',
-        accessKey: fromPropertiesFile(inGradleUserHome()),
     },
 }
